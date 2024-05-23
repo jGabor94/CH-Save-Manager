4b0542e44e7a2abcf84e63aa07afac6fa6986929
Jakucs <jakucs.gabor94@gmail.com>
2024-05-23 17:56:01 +0200

    Modify padding on landing page and center main title

--- src/app/@guest/page.tsx
+++ src/app/@guest/page.tsx
@@ -6,10 +6,10 @@ import GoogleSignInButton from "@/components/GoogleSignInButton";
 const Page: FC<{}> = () => {
     return (
         <Fragment>
-            <Stack sx={{ width: "100%", height: "80vh", position: "relative" }} alignItems="center" justifyContent="center" gap={3}>
+            <Stack sx={{ width: "100%", height: "80vh", position: "relative" }} alignItems="center" justifyContent="center" gap={3} p={2}>
                 <Stack alignItems="center">
                     <Box component="img" src="/ch_logo.png" sx={{ height: 120 }} />
-                    <Typography sx={{ fontWeight: 800, fontSize: 25 }}>Clicker Heroes Save Manager</Typography>
+                    <Typography sx={{ fontWeight: 800, fontSize: 25, textAlign: "center" }}>Clicker Heroes Save Manager</Typography>
                 </Stack>
                 <Typography sx={{ textAlign: "center" }}>
                     Store your saves in the cloud to access them from any device. You can later download the game files to any platform.
